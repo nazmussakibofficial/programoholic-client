de4c1b1ada99f87b748eb54f87a2792e64a830be
nazmussakibofficial <nazmussakibofficial19@gmail.com>
2022-10-26 08:28:43 +0600

    footer added

---
+++ src/shared/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div class=" bg-success">
+            <div class="max-w-2xl mx-auto text-white py-10">
+                <div class="text-center">
+                    <h3 class="text-3xl mb-3"> Download our app </h3>
+                    <p> Learn. Code. Conquer. </p>
+                    <div class="flex justify-center my-10">
+                        <div class="flex items-center  bg-white border border-white w-auto rounded-lg px-4 py-2 w-52 mx-2">
+                            <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8" alt='' />
+                            <div class="text-left text-black ml-3">
+                                <p class='text-xs'>Download on </p>
+                                <p class="text-sm md:text-base"> Google Play Store </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center bg-white border border-white w-auto rounded-lg px-4 py-2 w-44 mx-2">
+                            <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" class="w-7 md:w-8" alt='' />
+                            <div class="text-left text-black ml-3">
+                                <p class='text-xs'>Download on </p>
+                                <p class="text-sm md:text-base"> Apple Store </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+                    <p class="order-2 md:order-1 mt-8 md:mt-0 text-white"> &copy; Programoholic, 2022. </p>
+                    <div class="order-1 md:order-2">
+                        <span class="px-2 text-white">About us</span>
+                        <span class="px-2 text-white border-l">Contact us</span>
+                        <span class="px-2 text-white border-l">Privacy Policy</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/shared/Main.js
+++ src/shared/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 import Header from './Header';
 
 const Main = () => {
@@ -7,7 +8,7 @@ const Main = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
-
+            <Footer></Footer>
         </div>
     );
 };
